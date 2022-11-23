cd2003b4cdc61b4d212e34245d905945ddd06c76
Sanka Gallage <sankag@99x.io>
2022-11-24 05:01:52 +0530

    new styles and components added

---
+++ src/app/components/message-section/message-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-section',
+  templateUrl: './message-section.component.html',
+  styleUrls: ['./message-section.component.scss']
+})
+export class MessageSectionComponent {
+
+}
